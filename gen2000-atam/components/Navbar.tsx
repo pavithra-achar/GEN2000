57a2059a486f12c502c6c95cd5ca1d3fe167af8c
Ananya Jain <ananyaj1515@gmail.com>
2026-03-28 22:21:40 +0800

    Complete artist

--- gen2000-atam/components/Navbar.tsx
+++ gen2000-atam/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center bg-[#449495] px-5 py-2 text-white z-100">
   <div className="flex items-center gap-3">
-    <img src="./atam-logo.png" alt="ATAM Logo" className="h-10 w-10 object-contain" />
+    <img src="/atam-logo.png" alt="ATAM Logo" className="h-10 w-10 object-contain" />
     <span className="text-lg font-bold">ATAM Folk Arts Gallery</span>
   </div>
 </nav>
--- gen2000-atam/src/folkarts/Artist.tsx
+++ gen2000-atam/src/folkarts/Artist.tsx
@@ -14,14 +14,14 @@ const Artist = () => {
       <Background />
       <div className='relative'>
         <img className = "master-name"src="/master-name.png"/>
-        <img className="lotus"src="/lotus.png"/>
+       
         <img className='artist-frame' src="/frame1.png"/>
         <img className='leaf' src="/leaf.png"/>
         <img className='star' src="/star.png"/>
+        <img className='frame2' src="/frame2.png"/>
+         <img className='frame3' src="/frame2.png"/>
+          <img className="lotus"src="/lotus.png"/>
       </div>
-      
-      
-
     </div>
   )
 }
