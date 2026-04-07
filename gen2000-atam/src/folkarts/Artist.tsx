f70a24a594b459283ec6eff4b6d8f1ec666979af
Pavithra <pavithra.achar1505@gmail.com>
2026-04-07 11:56:16 +0800

    Font works now + home page is in grid format.

--- gen2000-atam/src/folkarts/Artist.tsx
+++ gen2000-atam/src/folkarts/Artist.tsx
@@ -9,26 +9,46 @@ const Artist = () => {
     id = "0";
   }
 
+  const numericId = parseInt(id);
+
   return (
-    <div className='page bg artist'>{folkarts.folkarts.at(parseInt(id))?.name}
+    <div className='page bg artist'>{folkarts.folkarts.at(numericId)?.name}
     
       <Background />
       <div className='relative'>
-        <img className = "master-name"src="/master-name.png"/>
-       <h3 className='font-grimp'>Why is this font not loading</h3>
+        <img className = "master-name" src="/master-name.png"/>
        <FrameCard
           frameSrc="/frame1.png"
-          imageSrc={folkarts.folkarts.at(parseInt(id))?.sections?.artists.content.frame1.imageSrc}
-          alt={folkarts.folkarts.at(parseInt(id))?.sections?.artists.content.frame1.alt}
-          modalTitle={folkarts.folkarts.at(parseInt(id))?.sections?.artists.content.frame1.modalTitle}
-          modalContent={folkarts.folkarts.at(parseInt(id))?.sections?.artists.content.frame1.modalContent}
+          imageSrc={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame1.imageSrc}
+          alt={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame1.alt}
+          modalTitle={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame1.modalTitle}
+          modalContent={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame1.modalContent}
           className="artist-frame"
           innerClassName=''
         />
-        <img className='artist-frame' src="/frame1.png"/>
+
         <img className='leaf' src="/leaf.png"/>
-        <img className='frame2' src="/frame2.png"/>
-         <img className='frame3' src="/frame2.png"/>
+        <FrameCard
+          frameSrc='=/frame2.png'
+          imageSrc={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame2?.imageSrc}
+          alt={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame2?.alt}
+          modalTitle={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame2?.modalTitle}
+          modalContent={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame2?.modalContent}
+          className="frame2"
+          innerClassName=''
+        />
+
+        <FrameCard
+          frameSrc='=/frame2.png'
+          imageSrc={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame3?.imageSrc}
+          alt={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame3?.alt}
+          modalTitle={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame3?.modalTitle}
+          modalContent={folkarts.folkarts.at(numericId)?.sections?.artists.content.frame3?.modalContent}
+          className="frame3"
+          innerClassName=''
+        />
+        {/* <img className='frame2' src="/frame2.png"/>
+         <img className='frame3' src="/frame2.png"/> */}
          <img className='star' src="/star.png"/>
           <img className="lotus"src="/lotus.png"/>
       </div>
